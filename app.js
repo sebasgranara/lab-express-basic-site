7f38d5868abc136e84b017345390eae19424e471
Sebas Granara <sebastian.granara@gmail.com>
2021-11-24 22:09:01 +0100

    no el mejor, pero interesante seguro

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.static('public'));
+
+  app.get('/home', (request, response, next) => response.sendFile(__dirname + '/views/home.html'));
+
+  app.get('/about', (request, response, next) => response.sendFile(__dirname + '/views/about.html'));
+
+  app.get('/works', (request, response, next) => response.sendFile(__dirname + '/views/works.html'));
+  
+  app.listen(3000, () => console.log('My first app listening on port 3000! '));
